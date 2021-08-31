2afdc2089995433ac926a4ed16c0aa972137a428
JusticeUA <vasya.ganec@gmail.com>
2021-08-31 22:39:48 +0300

    настроєний галп, без шрифтів, також проблема з плагіном imagemin, непрацює, невстановлюється

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ let path = {
         fonts: project_folder + "/fonts/",
     },
     scr:{
-        html: source_folder+"/*.html",
+        html: [source_folder+"/*.html", "!"+source_folder+"/_*.html" ],
         css: source_folder + "/scss/style.scss",
         js: source_folder + "/js/script.js",
         img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
@@ -29,10 +29,23 @@ let path = {
 
 let {src, dest} = require('gulp'),
     gulp = require('gulp'),
-    browsersync= require("browser-sync").create()
+    browsersync= require("browser-sync").create(),
+    del = require('del'),
+    scss = require('gulp-sass')(require('sass')),
+    autoprefixer = require('gulp-autoprefixer'),
+    group_media = require("gulp-group-css-media-queries"),
+    clean_css = require("gulp-clean-css"),
+    rename = require("gulp-rename"),
+    uglify = require("gulp-uglify-es").default,
+    //imagemin = require("gulp-imagemin"),
+    webp = require("gulp-webp"),
+    webphtml = require("gulp-webp-html"),
+    webpcss = require("gulp-webpcss")
+
+    
    
 
-function browserSync(param){
+function browserSync(param){  
     browsersync.init({
         server:{
             baseDir: "./" + project_folder + "/"
@@ -45,13 +58,96 @@ function browserSync(param){
 function html () {
     return src(path.scr.html)
     .pipe(fileinclude())
+    .pipe(webphtml())
     .pipe(dest(path.build.html))
     .pipe(browsersync.stream())
 }
 
-let build = gulp.series(html)
-let watch = gulp.parallel(build,browserSync)
+function css (params) {
+    return src(path.scr.css) 
+    .pipe(
+        scss({
+             outputStyle: 'expanded' }).on('error', scss.logError)
+        
+    ) 
+    .pipe(
+        group_media() /* шукаэм ы групуэмо медыа запити */
+        )
+    .pipe(
+        autoprefixer({
+            overrideBrowserslist: ["last 5 versions"],  /* підтримка 5 останніх версій браузера  */
+            cascade:true
+        })
+    ) 
+    .pipe(webpcss())
+    .pipe(dest(path.build.css))   /* вигружаєм норм файл css перед зжиманням */
+    .pipe(clean_css()) /*  чистим і зжимаємо файл css, важливо для оптимізації, швидше загружається */
+    .pipe(
+        rename({
+            extname: ".min.css"  /*перейменовуємо і добавляємо до назви мін */
+        })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream())
+}
 
+function js () {
+    return src(path.scr.js)
+    .pipe(fileinclude())   /*збирає різні js в 1  */
+    .pipe(dest(path.build.js))
+    .pipe(
+        uglify() /*зжимаємо js */
+    ) 
+    .pipe(
+        rename({
+            extname: ".min.js"  /*перейменовуємо і добавляємо до назви мін */
+        })
+    )
+    .pipe(dest(path.build.js))
+    
+    .pipe(browsersync.stream())
+}
+
+function images () {
+    return src(path.scr.img) 
+    .pipe(
+        webp({
+            quality: 70   // можна настроювати більше чи менше
+        })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(src(path.scr.img))
+ /*   .pipe(
+        imagemin({
+            progressive:true,
+            svgoPlugins:[{ removeViewBox: false }],
+            interlaced: true,
+            optimizationLevel: 3 // 0 to 7
+        })
+    )   */
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
+}
+
+function watchFiles(params) {
+    gulp.watch([path.watch.html], html),
+    gulp.watch([path.watch.css], css),
+    gulp.watch([path.watch.js], js)
+    gulp.watch([path.watch.img], images)
+
+}
+
+function clean (params) {
+    return del(path.clean)
+}
+
+let build = gulp.series(clean, gulp.parallel(js, css, html, images))
+let watch = gulp.parallel(build, watchFiles, browserSync)
+
+
+exports.images = images
+exports.js = js
+exports.css = css
 exports.html = html
 exports.build = build
 exports.watch = watch
